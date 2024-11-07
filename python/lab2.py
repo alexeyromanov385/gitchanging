5dcaefbc857e181c4d1f5a5eb09e45b9bc53ddee
Alexey Romanov <meta8304@gmail.com>
2024-11-07 19:07:09 +0700

    Another change1

--- python/lab2.py
+++ python/lab2.py
@@ -8,7 +8,7 @@ def display_menu():
     print("4. Удалить автомобиль")
     print("5. Поиск автомобиля")
     print("6. Выход из приложения")
-    print("ooo BSL 1235")
+    print("ooo BSL CORP")
 def main():
     file_name = 'cars.txt'
     
