cbe1fe1eb3d701ec71aa268d625842b6db49f8ee
Alexey Romanov <meta8304@gmail.com>
2024-11-12 22:03:23 +0700

    popa

--- python/lab2.py
+++ python/lab2.py
@@ -8,7 +8,7 @@ def display_menu():
     print("4. Удалить автомобиль")
     print("5. Поиск автомобиля")
     print("6. Выход из приложения")
-    print("ooo BSL CORPoration123567")
+    print("ooo BSL CORPoration1235678")
 def main():
     file_name = 'cars.txt'
     
